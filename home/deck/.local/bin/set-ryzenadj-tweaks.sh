ee92c2e7230436d5746d4622f4f59d9b551b76f6
Kyle Gospodnetich <me@kylegospodneti.ch>
2023-05-03 11:11:12 -0700

    Use hex value instead of decimal, same result just cleaner. More helpful comments

--- home/deck/.local/bin/set-ryzenadj-tweaks.sh
+++ home/deck/.local/bin/set-ryzenadj-tweaks.sh
@@ -2,11 +2,13 @@
 set -eu
 
 # https://github.com/NGnius/PowerTools/issues/84#issuecomment-1482736698
+# https://www.amd.com/system/files/documents/faq-curve-optimizer.pdf
+# Expect your UV to be 3-5x your set curve value. IE: -5 = -15mv to -25mv
 
 # CPU
 # 0x100000 - 5 (Range: -30, 30)
-/home/deck/.local/bin/ryzenadj --set-coall=1048571
+/home/deck/.local/bin/ryzenadj --set-coall=0xFFFFB
 
 # GPU
 # 0x100000 - 5 (Range: -30, 30)
-/home/deck/.local/bin/ryzenadj --set-cogfx=1048571
+/home/deck/.local/bin/ryzenadj --set-cogfx=0xFFFFB
