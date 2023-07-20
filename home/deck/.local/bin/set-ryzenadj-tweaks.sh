a8d3a9df489a72fd9274c1514f7d359fa8754558
GaleNorth <galenorth@gmail.com>
2023-07-20 21:41:32 +0200

    Update set-ryzenadj-tweaks.sh
    Set default UV back to -5

--- home/deck/.local/bin/set-ryzenadj-tweaks.sh
+++ home/deck/.local/bin/set-ryzenadj-tweaks.sh
@@ -8,11 +8,10 @@ allow=$(</home/deck/.local/bin/allowadj.txt)
 # Expect your UV to be 3-5x your set curve value. IE: -5 = -15mv to -25mv
 
 # CPU
-# 0x100000 - 22 (Range: -30, 30)
 if [[ $allow = "1" ]]
 then 
-    # 0x100000 - 22 (Range: -30, 30)
-    /home/deck/.local/bin/ryzenadj --set-coall=0xFFFED
+    # 0x100000 - 5 (Range: -30, 30)
+    /home/deck/.local/bin/ryzenadj --set-coall=0xFFFFB
     echo "Undervolt on" > /home/deck/.local/bin/statusadj.txt
 else
     # 0x100000 - 0
