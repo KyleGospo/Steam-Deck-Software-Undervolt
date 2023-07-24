4208eb9066dfd156224931be364a3a4d6571127f
GaleNorth <galenorth@gmail.com>
2023-07-24 15:15:28 +0200

    Update set-ryzenadj-tweaks.sh
    Code cleanup

--- home/deck/.local/bin/set-ryzenadj-tweaks.sh
+++ home/deck/.local/bin/set-ryzenadj-tweaks.sh
@@ -20,35 +20,35 @@ else
             echo "0" > /home/deck/.local/bin/experimentaladj.txt
 
             # Put experimental settings here - these
-            # will not be restored at next startup
+            # will never be restored at next startup
 
             # CPU
             # 0x100000 - 15 (Range: -30, 30)
             /home/deck/.local/bin/ryzenadj --set-coall=0xFFFF0
 
-            # GPU
+            # GPU (Currently not working!)
             # 0x100000 - 15 (Range: -30, 30)
-            /home/deck/.local/bin/ryzenadj --set-cogfx=0xFFFF0
+            # /home/deck/.local/bin/ryzenadj --set-cogfx=0xFFFF0
 
             echo "Experimental on" > /home/deck/.local/bin/statusadj.txt
         else
-            # Mark apply in progress, failsafe to avoid repeated crashes at startup
+            # Fail safe to avoid repeated crashes at startup
             echo "Applying undervolt" > home/deck/.local/bin/statusadj.txt
             
             # Put verified settings here.
             # WARNING: when service is enabled these will be restored
             # at next startup and can make your device unaccessible until you
             # repair/reimage your deck!
-
+            
             # CPU
             # 0x100000 - 5 (Range: -30, 30)
             /home/deck/.local/bin/ryzenadj --set-coall=0xFFFFB
 
-            # GPU
+            # GPU (Currently not working!)
             # 0x100000 - 5 (Range: -30, 30)
-            /home/deck/.local/bin/ryzenadj --set-cogfx=0xFFFFB
+            # /home/deck/.local/bin/ryzenadj --set-cogfx=0xFFFFB
 
-            # Wait 10 seconds before declaring success
+            # Wait 10 seconds before declaring the undervolt a success
             sleep 10
             echo "Undervolt on" > /home/deck/.local/bin/statusadj.txt
         fi
@@ -56,14 +56,14 @@ else
         # Put default values here so the off.sh script can disable your tweaks.
         # If you have experimental settings you forget to restore here a restart
         # of your deck will also put the values back to default
-
+        
         # CPU
         # 0x100000 - 0
         /home/deck/.local/bin/ryzenadj --set-coall=0x100000
 
-        # GPU
+        # GPU (Currently not working!)
         # 0x100000 - 0
-        /home/deck/.local/bin/ryzenadj --set-cogfx=0x100000
+        # /home/deck/.local/bin/ryzenadj --set-cogfx=0x100000
 
         echo "Undervolt off" > /home/deck/.local/bin/statusadj.txt
     fi
